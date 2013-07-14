585a5a040d26c858509375fa08bee4e7cf273b54
Anubhav Agarwal <anubhav914@gmail.com>
2013-07-15 03:01:05 +0530

    Implemented a checkbox for making an edit as spam

---
+++ BayesianFilter.i18n.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Internationalisation file for extension BayesianFilter.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+$messages['en'] = array(
+						'flag-spam-check-title' => "Mark this checkbox if you think that the edit that you are undoing is a spam",
+						'flag-spam-check' => "Mark this as Spam"
+
+	);
+
